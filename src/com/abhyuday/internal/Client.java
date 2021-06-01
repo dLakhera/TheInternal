25ed1615646d73e58d257f49bc13f01f8b62ee1d
Deepak Lakhera <dlakhera02@gmail.com>
2021-06-01 18:27:47 +0530

    Message box and send button added

--- src/com/abhyuday/internal/Client.java
+++ src/com/abhyuday/internal/Client.java
@@ -7,7 +7,14 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.GridBagLayout;
 import javax.swing.JTextArea;
+import javax.swing.UIManager;
+
 import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Client extends JFrame {
 
@@ -16,7 +23,9 @@ public class Client extends JFrame {
 
 	private String name,address;
 	private int port;
-	
+	private JTextField txtMessage;
+		
+	private JTextArea txtrHistory;
 	/**
 	 * Create the frame.
 	 */
@@ -27,9 +36,18 @@ public class Client extends JFrame {
 		this.address = address;
 		this.port = port;
 		createWindow();
+		console("Successfully connected!");
+		console("For the user:"+ name + " on the address: " + address + ":" + port + "!");
 	}
 	
 	private void createWindow() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 550);
 		setLocationRelativeTo(null);
@@ -37,19 +55,52 @@ public class Client extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{10, 780, 10};
+		gbl_contentPane.columnWidths = new int[]{34, 726, 33, 7};
 		gbl_contentPane.rowHeights = new int[]{50, 450, 50};
-		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWeights = new double[]{1.0, 1.0};
 		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
-		JTextArea txtrHistory = new JTextArea();
+		txtrHistory = new JTextArea();
+		txtrHistory.setEditable(false);
 		GridBagConstraints gbc_txtrHistory = new GridBagConstraints();
+		gbc_txtrHistory.gridwidth = 2;
+		gbc_txtrHistory.insets = new Insets(0, 0, 5, 5);
 		gbc_txtrHistory.fill = GridBagConstraints.BOTH;
 		gbc_txtrHistory.gridx = 1;
 		gbc_txtrHistory.gridy = 1;
+		// gbc_txtrHistory.insets = new Insets(0, 20, 0, 0);
 		contentPane.add(txtrHistory, gbc_txtrHistory);
+		
+		txtMessage = new JTextField();
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 1;
+		gbc_txtMessage.gridy = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Send");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String message = txtMessage.getText();
+				console(name + ": " +message);
+				txtMessage.setText("");
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 2;
+		gbc_btnNewButton.gridy = 2;
+		contentPane.add(btnNewButton, gbc_btnNewButton);
 		setVisible(true);
+		
+		txtMessage.requestFocusInWindow();
 	}
-
+	
+	public void console(String message) {
+		txtrHistory.append(message + "\n\r");
+	}
+	
 }
